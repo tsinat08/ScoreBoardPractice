ab1ab6fc98f6d2ea8bb10cb8d05e44d198087aa4
tsinat08 <tsinat.weldetnesae@gmail.com>
2019-06-03 12:52:55 -0400

    Practice

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 var main = function() {
-
+   $('.more-btn').on('click', (event) =>{
+       $(event.currentTarget).siblings('.more-menu').toggle();
+   });
+    $('.share').on('click', (event) =>{
+        $(event.currentTarget).next('.share-menu').toggle();
+    });
+    S('.notification').on('click', (event) =>{
+       $(event.currentTarget).toggle('active');
+    })
 };
 $(document).ready(main);
